99a1f7b614c9f52b459cd137570d048eb66ed3a4
Takuya Matsuyama <hi@craftz.dog>
2021-09-15 16:32:45 -0400

    production

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -1,21 +1,22 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['djangoblogtemplate.herokuapp.com']
+ALLOWED_HOSTS = ['template-for-django-blog.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbffhn4tvch48q',
+        'NAME': 'ddl9k07mlf85gh',
         'PORT': 5432,
-        'USER': 'ltyxzztyhxxsqo',
-        'PASSWORD': 'af09e2e34e581fc962eac7adb4607124653a2d9d13e4f46e201f0636c7bc84bc',
-        'HOST': 'ec2-44-198-146-224.compute-1.amazonaws.com'
+        'USER': 'sgejenqmzqaqrk',
+        'PASSWORD': '7659dfb7917e2246f729f0da9efd90c810a26f8fc555acec670c7cb7e5344bd2',
+        'HOST': 'ec2-34-228-154-153.compute-1.amazonaws.com'
     }
 }
 
 STATICFILES_DIRS = (BASE_DIR, 'static')
+STATIC_ROOT = 'template-for-django-blog/heroku.com/static'
--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings.production')
 
 from dj_static import Cling
 application = Cling(get_wsgi_application())
